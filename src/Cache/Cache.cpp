408014010209fe4434c3f8e26687bdb3486cd555
Lucase <lucasfba13@gmail.com>
2025-01-31 19:03:29 -0300

    cache2

--- src/Cache/Cache.cpp
+++ src/Cache/Cache.cpp
@@ -1,5 +1,5 @@
 #include "../includes/Cache.hpp"
 
-Cache::Cache() : capacidade(3) {}
+Cache::Cache() : capacidade(5) {}
 
 Cache::Cache(int capacidade) : capacidade(capacidade) {}
--- src/includes/Cache.hpp
+++ src/includes/Cache.hpp
@@ -4,42 +4,46 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <list>
 
 using namespace std;
 
 class Cache {
 private:
-    vector<pair<string, int>> cache; 
+    unordered_map<string, int> cacheMap; 
+    list<string> order; 
     int capacidade; 
 
 public:
     Cache(); 
     Cache(int capacidade); 
 
-    void set(const string& instruction, int result) {
-        if (cache.size() >= capacidade) {
+    void set(const std::string& instruction, int result) {
+        if (cacheMap.size() >= capacidade) {
             cout << "Cache cheia, removendo a instrução mais antiga." << endl;
-            cache.erase(cache.begin()); // FIFO
-            return;
+            std::string oldest = order.front(); 
+            order.pop_front(); 
+            cacheMap.erase(oldest); 
         }
-        cache.push_back({instruction, result});
+        cacheMap[instruction] = result; 
+        order.push_back(instruction); 
     }
 
-    bool get(const string& instruction, int& result) {
-        for (const auto& entry : cache) {
-            if (entry.first == instruction) { // verifica se a instrucao esta na cache
-                result = entry.second; 
-                return true;
-            }
+    bool get(const std::string& instruction, int& result) {
+        auto it = cacheMap.find(instruction);
+        if (it != cacheMap.end()) {
+            result = it->second; 
+            return true; 
         }
-        return false;
+        return false; 
     }
 
     void printCache() {
         cout << "Estado atual da Cache:" << endl;
-        for (const auto& entry : cache) {
-            cout << "Instrução: " << entry.first 
-                 << ", Resultado: " << entry.second << endl;
+        for (const auto& instruction : order) {
+            cout << "Instrução: " << instruction 
+                << ", Resultado: " << cacheMap[instruction] << endl;
         }
     }
 };

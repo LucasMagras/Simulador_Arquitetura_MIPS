2485e65d3ff5c8f576afe0582367b713196cf00a
LucasMagras <lucasfba13@gmail.com>
2025-01-31 19:25:13 -0300

    cache3

--- src/Cache/Cache.cpp
+++ src/Cache/Cache.cpp
@@ -3,3 +3,32 @@
 Cache::Cache() : capacidade(5) {}
 
 Cache::Cache(int capacidade) : capacidade(capacidade) {}
+
+void Cache::set(const std::string& instruction, int result) {
+        if (cacheMap.size() >= capacidade) {
+            cout << "Cache cheia, removendo a instrução mais antiga." << endl;
+            std::string oldest = order.front(); 
+            order.pop_front(); 
+            cacheMap.erase(oldest); 
+        }
+        cacheMap[instruction] = result; 
+        order.push_back(instruction); 
+}
+
+bool Cache::get(const std::string& instruction, int& result) {
+        auto it = cacheMap.find(instruction);
+        if (it != cacheMap.end()) {
+            result = it->second; 
+            return true; 
+        }
+        return false; 
+}
+
+void Cache::printCache() {
+        cout << "Estado atual da Cache:" << endl;
+        for (const auto& instruction : order) {
+            cout << "Instrução: " << instruction 
+                << ", Resultado: " << cacheMap[instruction] << endl;
+        }
+}
+
--- src/includes/Cache.hpp
+++ src/includes/Cache.hpp
@@ -19,33 +19,9 @@ public:
     Cache(); 
     Cache(int capacidade); 
 
-    void set(const std::string& instruction, int result) {
-        if (cacheMap.size() >= capacidade) {
-            cout << "Cache cheia, removendo a instrução mais antiga." << endl;
-            std::string oldest = order.front(); 
-            order.pop_front(); 
-            cacheMap.erase(oldest); 
-        }
-        cacheMap[instruction] = result; 
-        order.push_back(instruction); 
-    }
-
-    bool get(const std::string& instruction, int& result) {
-        auto it = cacheMap.find(instruction);
-        if (it != cacheMap.end()) {
-            result = it->second; 
-            return true; 
-        }
-        return false; 
-    }
-
-    void printCache() {
-        cout << "Estado atual da Cache:" << endl;
-        for (const auto& instruction : order) {
-            cout << "Instrução: " << instruction 
-                << ", Resultado: " << cacheMap[instruction] << endl;
-        }
-    }
+    void set(const std::string& instruction, int result);
+    bool get(const std::string& instruction, int& result);
+    void printCache();
 };
 
 #endif
